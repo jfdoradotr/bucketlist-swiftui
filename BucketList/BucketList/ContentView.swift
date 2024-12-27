64e3e73e434b92fcbaaacce2331850d31b02dd30
Juan Dorado <jfdoradotr@icloud.com>
2024-12-27 14:23:18 -0600

    Add the location to the map as a marker

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -12,18 +12,22 @@ struct ContentView: View {
       span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
     )
   )
-
+  
   @State private var locations = [Location]()
-
+  
   var body: some View {
     MapReader { proxy in
-      Map(initialPosition: startPosition)
-        .onTapGesture { position in
-          if let coordinate = proxy.convert(position, from: .local) {
-            let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
-            locations.append(newLocation)
-          }
+      Map(initialPosition: startPosition) {
+        ForEach(locations) { location in
+          Marker(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
         }
+      }
+      .onTapGesture { position in
+        if let coordinate = proxy.convert(position, from: .local) {
+          let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+          locations.append(newLocation)
+        }
+      }
     }
   }
 }
