ec8abaa7e173f63dd6d4926a2c25c36fe1de61ad
Juan Dorado <jfdoradotr@icloud.com>
2024-12-27 16:44:14 -0600

    Create the initializer to decode the locations from disk

--- BucketList/BucketList/ContentView.Model.swift
+++ BucketList/BucketList/ContentView.Model.swift
@@ -10,6 +10,16 @@ extension ContentView {
   class Model {
     private(set) var locations = [Location]()
     var selectedPlace: Location?
+    let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+
+    init() {
+      do {
+        let data = try Data(contentsOf: savePath)
+        locations = try JSONDecoder().decode([Location].self, from: data)
+      } catch {
+        locations = []
+      }
+    }
 
     func addLocation(at point: CLLocationCoordinate2D) {
       let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: point.latitude, longitude: point.longitude)
