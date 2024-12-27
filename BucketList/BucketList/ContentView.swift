5b968b1d39ebd60b8c850185b0b92a0376726048
Juan Dorado <jfdoradotr@icloud.com>
2024-12-27 14:36:25 -0600

    Show a sheet if we select a place

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
   )
 
   @State private var locations = [Location]()
+  @State private var selectedPlace: Location?
 
   var body: some View {
     MapReader { proxy in
@@ -34,6 +35,9 @@ struct ContentView: View {
           let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
           locations.append(newLocation)
         }
+      }
+      .sheet(item: $selectedPlace) { place in
+        Text(place.name)
       }
     }
   }
