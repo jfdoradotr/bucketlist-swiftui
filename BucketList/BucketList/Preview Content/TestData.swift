187d132715b44b0f7b5274ecc4fa842f7caa3ba1
Juan Dorado <jfdoradotr@icloud.com>
2024-12-27 14:33:20 -0600

    Create Test Data as part of only the preview

---
+++ BucketList/BucketList/Preview Content/TestData.swift
@@ -0,0 +1,15 @@
+//	
+// Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import Foundation
+
+extension Location {
+  static let example = Location(
+    id: UUID(),
+    name: "Buckingham Palace",
+    description: "Lit by over 40,000 lightbulbs",
+    latitude: 51.501,
+    longitude: -0.141
+  )
+}
